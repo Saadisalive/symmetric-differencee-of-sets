4f3a5c5a02fa75b2e01d2190c0ec9fc1c1a3690b
Saadisalive <snafiullah@gmail.com>
2025-05-10 21:16:19 +0500

    Add files via upload

---
+++ Symmetric difference between sets.py
@@ -0,0 +1,18 @@
+set1 = {'blue','green'}
+set2 = {'blue','yellow'}
+print(set1)
+print(set2)
+
+print("\nIntersection between two sets:")
+set3 = set1.intersection(set2)
+print(set3)
+
+print("\nNow partB")
+setx = {1,2,3,4,5}
+sety = {1,5,6,7,8,9}
+print(setx)
+print(sety)
+
+print("\n Intersection between two numbers sets:")
+setz = setx.intersection(sety)
+print(setz)
